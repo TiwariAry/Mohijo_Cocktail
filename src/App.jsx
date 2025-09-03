03be3285d76cce60a2b18f67d4321446c44b5acc
Aryan Tiwari <sujatatiwari151@gmail.com>
2025-09-03 22:18:37 +0530

    Add first Hero section part

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from 'gsap'
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 
 // Activate the plugins
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
     return (
-        <Navbar/>
+        <main>
+            <Navbar/>
+            <Hero/>
+            <div className={"h-dvh bg-black"}></div>
+        </main>
     )
 }
 
